a9858f3573810656633761eaa6021b617d8f4917
uudx <155887302+uudx@users.noreply.github.com>
2025-03-04 17:55:08 +0500

    Create 2task.py

---
+++ dir-and-files/2task.py
@@ -0,0 +1,6 @@
+import os
+path = 'D:\\projects\\data.txt'
+print('Exist:', os.access(path, os.F_OK))
+print('Readable:', os.access(path, os.R_OK))
+print('Writable:', os.access(path, os.W_OK))
+print('Executable:', os.access(path, os.X_OK))
